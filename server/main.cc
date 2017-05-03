0a69b069c8bd554b034c7801e510d3a3bc46edd0
tattsun <t.t.mc1192.sf@gmail.com>
2017-05-04 00:04:49 +0900

    simple packet receiver

--- server/main.cc
+++ server/main.cc
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <sstream>
+#include <memory>
+#include <cstdlib>
 
 #include <pcap.h>
 #include <netinet/ip.h>
@@ -7,7 +10,65 @@
 
 using namespace std;
 
+struct IPPacket {
+  struct pcap_pkthdr header;
+  const unsigned char* packet;
+};
+
+class PacketReceiver {
+public:
+  PacketReceiver(const string& dev)
+    : dev(dev) {
+  }
+
+  void start(string& err) {
+    if((handle = pcap_open_live(dev.c_str(), bufsiz, 1, 1000, errbuf)) == NULL) {
+      ostringstream st;
+      st << "couldn't open device " << dev << errbuf;
+      err = st.str();
+      return;
+    }
+
+    if(pcap_datalink(handle) != DLT_EN10MB) {
+      ostringstream st;
+      st << "device not support " << dev;
+      err = st.str();
+      return;
+    }
+  }
+
+  unique_ptr<IPPacket> recv() {
+    unique_ptr<IPPacket> pkt(new IPPacket());
+    while((pkt->packet = pcap_next(handle, &pkt->header)) == NULL) {
+      if(pkt->header.len < sizeof(struct ether_header)+sizeof(struct ip)) {
+        continue;
+      }
+    }
+    return pkt;
+  }
+private:
+  static int bufsiz;
+    
+  string dev;
+  char errbuf[1024];
+  
+  pcap_t* handle;
+};
+int PacketReceiver::bufsiz = 1024;
 
 int main() {
-  cout << "Unpacket server" << endl;
+  string err;
+  
+  unique_ptr<PacketReceiver> receiver(new PacketReceiver("en0"));
+  receiver->start(err);
+  if(!err.empty()) {
+    cerr << "ERR: " << err << endl;
+    abort();
+  }
+
+  unique_ptr<IPPacket> pkt;
+  while(1) {
+    pkt = receiver->recv();
+    cout << "Received" << endl;
+  }
 }
